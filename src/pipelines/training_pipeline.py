c3f9b277383325599381495e2500dfbfda1fc393
rajnish203 <rajnishdivakar618@gmail.com>
2024-02-19 16:23:46 +0530

    project uploading

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,280 @@
+
+from src.logger import logging
+from src.exception import CustomException
+
+from src.components.data_ingestion import DataIngestion
+
+from sklearn.impute import SimpleImputer ## HAndling Missing Values
+from sklearn.preprocessing import StandardScaler # HAndling Feature Scaling
+from sklearn.preprocessing import OrdinalEncoder # Ordinal Encoding
+## pipelines
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+import sys,os
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+from src.exception import CustomException
+from src.logger import logging
+# Basic Import
+from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from src.exception import CustomException
+from src.logger import logging
+from dataclasses import dataclass
+import sys
+import os
+
+
+
+import pickle
+import numpy as np 
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            
+
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
+
+            # Get R2 scores for train and test data
+            #train_model_score = r2_score(ytrain,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] =  test_model_score
+
+        return report
+    
+    except Exception as e:
+            logging.info('Exception occured during model training')
+            raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
+
+@dataclass 
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initate_model_training(self,train_array,test_array):
+        try:
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models={
+            'LinearRegression':LinearRegression(),
+            'Lasso':Lasso(),
+            'Ridge':Ridge(),
+            'Elasticnet':ElasticNet(),
+            'DecisionTree':DecisionTreeRegressor()
+        }
+            
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
+            print(model_report)
+            print('\n====================================================================================\n')
+            logging.info(f'Model Report : {model_report}')
+
+            # To get best model score from dictionary 
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
+            best_model = models[best_model_name]
+
+            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+
+            save_object(
+                 file_path=self.model_trainer_config.trained_model_file_path,
+                 obj=best_model
+            )
+          
+
+        except Exception as e:
+            logging.info('Exception occured at Model Training')
+            raise CustomException(e,sys)
+
+
+## Data Transformation config
+
+@dataclass
+class DataTransformationconfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+
+
+## Data Ingestionconfig class
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationconfig()
+
+    def get_data_transformation_object(self):
+         
+         try:
+            logging.info('Data Transformation initiated')
+            # Define which columns should be ordinal-encoded and which should be scaled
+            categorical_cols = ['cut', 'color','clarity']
+            numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
+            
+            # Define the custom ranking for each ordinal variable
+            cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
+            color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
+            clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+            
+            logging.info('Pipeline Initiated')
+
+            ## Numerical Pipeline
+            num_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='median')),
+                ('scaler',StandardScaler())
+
+                ]
+
+            )
+
+            # Categorigal Pipeline
+            cat_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
+                ('scaler',StandardScaler())
+                ]
+
+            )
+
+            preprocessor=ColumnTransformer([
+            ('num_pipeline',num_pipeline,numerical_cols),
+            ('cat_pipeline',cat_pipeline,categorical_cols)
+            ])
+            
+            return preprocessor
+
+            logging.info('Pipeline Completed')
+
+         except Exception as e:
+            
+            logging.info("Error in Data Trnasformation")
+            raise CustomException(e,sys)
+
+
+
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            # Reading train and test data
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info('Read train and test data completed')
+            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head  : \n{test_df.head().to_string()}')
+
+            logging.info('Obtaining preprocessing object')
+
+            preprocessing_obj = self.get_data_transformation_object()
+
+            target_column_name = 'price'
+            drop_columns = [target_column_name,'id']
+
+            ## features into independent and dependent features
+
+            input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            ## apply the transformation
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            logging.info("Applying preprocessing object on training and testing datasets.")
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+
+            logging.info('Processsor pickle in created and saved')
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise CustomException(e,sys)
+
+
+    
+
+
+
+
+
+
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    print(train_data_path,test_data_path)
+
+    data_transformation=DataTransformation()
+
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
+
